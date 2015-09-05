235f80539aa3b7cadafcd40dafaea723fc4386b5
k352ex <k352ex@naver.com>
2015-09-06 03:15:53 +0900

    devise 연동 및 bootstrap 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  root to: 'post#index'
+  devise_for :users, controllers: { sessions: "users/sessions" }
+
+  resources :post
+
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'post#index', as: :authenticated_root
+    end
+
+    unauthenticated :user do
+      root :to => "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
 end
