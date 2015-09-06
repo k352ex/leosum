a8b4cbd5337e9dee294c495fa657eae8f2fe61a5
k352ex <k352ex@naver.com>
2015-09-06 13:51:27 +0900

    이미지 업로드 폼 추가

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,17 +2,18 @@ class PostController < ApplicationController
   def index
 
   end
-
   def create
-
     @post = Post.new
     @post.title  = params[:title]
     @post.artist = params[:artist]
     @post.context = params[:context]
     @post.image  = params[:image]
     @post.save
-
     redirect_to authenticated_root_path
+
+  end
+
+  def new
 
   end
 
