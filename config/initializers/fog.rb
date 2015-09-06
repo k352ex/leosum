637fc4738ff0ecdd1370ac828d2178a428183246
k352ex <k352ex@naver.com>
2015-09-06 14:35:47 +0900

    AWS S3연동 완료

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAJD2KWOWTSUH7VYDA',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     'AKIAJOMUA6V7RGEQ6GTQ',                        # required
+    aws_secret_access_key: '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',                        # required
     region:                'ap-northeast-1'
   }
-  config.fog_directory  = 'leosum'                          # required
+  config.fog_directory  = '/leosum'                          # required
 end
