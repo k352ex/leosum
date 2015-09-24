e9c15d50caab688a37d44db0fc269a315dd43caa
k352ex <k352ex@naver.com>
2015-09-24 13:32:08 +0900

    아티스트 및 이슈 페이지 word cloude 적용

--- db/schema.rb
+++ db/schema.rb
@@ -88,7 +88,6 @@ ActiveRecord::Schema.define(version: 20150921023063) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               limit: 255
-    t.string   "keyword",                limit: 255
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
